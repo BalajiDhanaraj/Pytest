7f97687b05448219b331818f93e7021a876ffd87
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-22 11:29:32 +0530

    updated

--- Test/config.py
+++ Test/config.py
@@ -2,20 +2,20 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-# @pytest.fixture(params=["chrome","firefox"],scope="function")
-# def get_browser(request):
-#
-#     if request.param == "chrome":
-#         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-#     request.cls.driver = driver
-#     driver.maximize_window()
-#     driver.implicitly_wait(10)
-#     yield driver
-#     driver.quit()
+@pytest.fixture(params=["chrome","firefox"],scope="function")
+def get_browser(request):
+
+    if request.param == "chrome":
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    request.cls.driver = driver
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
 
 
-@pytest.fixture
-def input_value():
-   input = 39
-   return input
+# @pytest.fixture
+# def input_value():
+#    input = 39
+#    return input
 
