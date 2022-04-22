264aa4bb74eb52d46149efedb4194ff3b3ae43b2
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-22 13:03:38 +0530

    updated

--- Test/conftest.py
+++ Test/conftest.py
@@ -2,6 +2,11 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+
+
+""" The conftest.py file name should be conftest only, otherwise python doesnt understand the file, eg - config.py there is no "test" keywords is involved in it. """
+
+
 @pytest.fixture(params=["chrome"],scope="class")
 def get_browser(request):
 
--- Test/test_demo.py
+++ Test/test_demo.py
@@ -4,12 +4,31 @@ from selenium import webdriver
 
 
 
+# @pytest.mark.usefixtures("get_browser")
+# class BaseTest:
+#     pass
+#
+# class Test_google(BaseTest):
+#
+#     def test_chrome(self):
+#         self.driver.get("http://www.google.com")
+#         assert self.driver.title == "Google"
+from selenium.webdriver.common.by import By
+
+
 @pytest.mark.usefixtures("get_browser")
 class BaseTest:
     pass
 
 class Test_google(BaseTest):
-
-    def test_chrome(self):
-        self.driver.get("http://www.google.com")
-        assert self.driver.title == "Google"
+    @pytest.mark.parametrize(
+                "username,password",
+                [
+                    ("admin@gamil.com","admin@123"),
+                    ("naveen@gmail.com","balaji@123")
+                ]
+    )
+    def test_chrome(self,username,password):
+        self.driver.get("https://www.instagram.com/?hl=en")
+        self.driver.find_element(by=By.XPATH,value="//input[@name='username']").send_keys(username)
+        self.driver.find_element(by=By.XPATH, value="//input[@name='password']").send_keys(password)
