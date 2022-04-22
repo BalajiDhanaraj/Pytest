e647c9502e2f9a51b1cb9b83e7b447ba457dcbfc
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-22 11:37:13 +0530

    updated

--- Test/config.py
+++ Test/config.py
@@ -2,16 +2,30 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(params=["chrome","firefox"],scope="function")
-def get_browser(request):
+#
+# class Base():
+#
+#     @pytest.fixture(params=["chrome", "firefox"], scope="function")
+#     def get_browser(request):
+#         if request.param == "chrome":
+#             driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+#         request.cls.driver = driver
+#         driver.implicitly_wait(10)
+#         yield driver
+#         driver.close()
 
-    if request.param == "chrome":
-        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    request.cls.driver = driver
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
+
+class BaseTest():
+
+    @pytest.fixture(scope="class")
+    def get_browser(request):
+        if request.param == "chrome":
+            driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        request.cls.driver = driver
+        driver.implicitly_wait(10)
+        yield driver
+        driver.close()
+
 
 
 # @pytest.fixture
--- Test/test_demo.py
+++ Test/test_demo.py
@@ -8,9 +8,6 @@ from selenium import webdriver
 
 @pytest.mark.usefixtures("get_browser")
 class BaseTest:
-    pass
-
-class Test_google(BaseTest):
 
     def test_chrome(self):
         self.driver.get("http://www.google.com")
